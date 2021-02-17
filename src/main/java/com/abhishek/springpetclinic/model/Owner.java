f4d59414e5e758c1fdf5115b2df59e833590333f
ABHISHEK KUMAR JAIN <theabhidot@gmail.com>
2021-02-18 01:36:59 +0530

    added POJOs. Closes #2.

---
+++ src/main/java/com/abhishek/springpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.abhishek.springpetclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ src/main/java/com/abhishek/springpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.abhishek.springpetclinic.model;
+
+public class Vet extends Person{
+
+}
